a062c48d09ddc54fb777303026b555279b9e4a88
ChristianMay21 <45889402+ChristianMay21@users.noreply.github.com>
2019-05-27 19:36:04 -0400

    Fixed app.js, removed app2.js

--- app.js
+++ app.js
@@ -1,88 +1,64 @@
-
-var mysql = require('mysql'); //imports mysql module
+var mysql = require('mysql');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 var loginInfo = new Object(); //objects in JavaScript are more like dictionaries
 
-var questions = [
+var questions = [ //list of questions to ask
     "host (default localhost): ",
     "user (default root): ",
-    "password: "
+    "password: ",
+    "database name: "
 ];
 var answers = [];
 
-function ask(i) {
-    process.stdout.write(`\n ${questions[i]}`);
-    process.stdout.write("\n  >  ");
+getInput = (callback) => {
+    rl.question(questions[0], function (ans) {
+        answers.push(ans);
+        rl.setPrompt(questions[1]);
+        rl.prompt();
+        rl.on('line', function(ans) {
+            if(answers.length < questions.length -1) {
+                answers.push(ans.trim());
+                rl.setPrompt(questions[answers.length]);
+                rl.prompt();
+            } else if (answers.length = questions.length -1) {
+                answers.push(ans.trim());
+                rl.close();
+                callback();
+            }
+        });
+    });
 }
 
-process.stdin.on('data', function(data) {
-    console.log('detected data');
-    answers.push(data.toString().trim());
-    if (answers.length < questions.length) {
-        ask(answers.length);
-    } else {
-        storeLoginInfo();
-        process.exit();
-    }
-});
+function main(callback = null) {
 
+    getInput(() => {
+        if (callback != null) {
+            callback();
+        }
+    });
+}
 
-function storeLoginInfo() {
-    console.log("storing login info");
+function connect() {
     loginInfo.host = answers[0];
     loginInfo.user = answers[1];
     loginInfo.password = answers[2];
     loginInfo.database = answers[3];
-    establishConnection();
-}
+    var connection = mysql.createConnection(loginInfo);
+    connection.connect();
 
-function establishConnection() {
-    console.log("Collected login information: ")
-    console.log(loginInfo);
-    console.log("Attempting server connection with provided information");
-//    var connection = mysql.createConnection(loginInfo);
-    var connection = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : 'password',
-        database: 'dnddb'
-    });
-
-    connection.connect(function(err) {
-        if (err) {
-            console.err("error connecting: " + err.stack);
-        }
-        console.log('connected as id ' + connection.threadId);
-    });
-
-    
     connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
         if (error) throw error;
-        console.log('The solution is: ', results[0].solution);
+        if(results[0].solution === 2) {
+            console.log('Test query successful');
+        }
+        console.log("Connection state: " + connection.state);
     });
-
-    if(connection.state === 'connected') {
-        console.log("Successfully connected!");
-    } else {
-        console.log(`Connection unsuccessful: connection state is [${connection.state}]`);
-    }
+    connection.end();
 }
 
-
-ask(0);
-/*rl.question("host (default localhost): ", (answer) => { //user input to insert hostname
-    console.log(answer);
-    loginInfo.host = answer;
-    rl.close();
-});
-
-rl.question("user (default root): ", (answer) => { //user input to input user name
-    loginInfo.user = answer;
-    rl.close();
-});
-
-rl.question("password: ", (answer) => { //user input to input answer
-    loginInfo.password = answer;
-    rl.close();
-});
-*/
+main(connect);
--- app2.js
+++ app2.js
@@ -1,17 +1,64 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'dnddb'
+var mysql = require('mysql');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
- 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-  console.log(connection.state);
-});
- 
-connection.end();
+
+var loginInfo = new Object(); //objects in JavaScript are more like dictionaries
+
+var questions = [ //list of questions to ask
+    "host (default localhost): ",
+    "user (default root): ",
+    "password: ",
+    "database name: "
+];
+var answers = [];
+
+getInput = (callback) => {
+    rl.question(questions[0], function (ans) {
+        answers.push(ans);
+        rl.setPrompt(questions[1]);
+        rl.prompt();
+        rl.on('line', function(ans) {
+            if(answers.length < questions.length -1) {
+                answers.push(ans.trim());
+                rl.setPrompt(questions[answers.length]);
+                rl.prompt();
+            } else if (answers.length = questions.length -1) {
+                answers.push(ans.trim());
+                rl.close();
+                callback();
+            }
+        });
+    });
+}
+
+function main(callback = null) {
+
+    getInput(() => {
+        if (callback != null) {
+            callback();
+        }
+    });
+}
+
+function connect() {
+    loginInfo.host = answers[0];
+    loginInfo.user = answers[1];
+    loginInfo.password = answers[2];
+    loginInfo.database = answers[3];
+    var connection = mysql.createConnection(loginInfo);
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw error;
+        if(results[0].solution === 2) {
+            console.log('Test query successful');
+        }
+        console.log("Connection state: " + connection.state);
+    });
+    connection.end();
+}
+
+main(connect);
